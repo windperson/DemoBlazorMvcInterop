229483cba00260ba3f20c76aed7ad0a1bf947df4
Chen-Yu Pao <windperson@gmail.com>
2020-12-15 19:09:20 +0800

    Demo Blazor jump out to call MVC action method

---
+++ DemoBlazorMvcInterop/Controllers/FinalResultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoBlazorMvcInterop.Controllers
+{
+    public class FinalResultController : Controller
+    {
+        // GET
+        public IActionResult Total(int result)
+        {
+            ViewData["TotalCount"] = result;
+            return View();
+        }
+    }
+}
